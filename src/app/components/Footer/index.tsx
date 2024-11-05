670777495cf02a5db6a19e26ae911de800379f15
Jagan <jagan@everbee.io>
2024-11-05 08:46:36 +0530

    fix: add keys to all loops

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,61 +1,70 @@
-'use client'
-import Logo from "../Logo";
-import './index.css'
-import { Divider } from "./Divider";
+'use client';
+import Logo from '../Logo';
+import './index.css';
+import { Divider } from './Divider';
 
 export function Footer() {
+  const supportPageLinks = () => {
+    let pages = ['About Us', 'Services', 'Contact Us', 'FAQ', 'Blog'];
+    return pages.map((page) => (
+      <div key={page} className="page-link">
+        {page}
+      </div>
+    ));
+  };
+  const socialLinks = () => {
+    let links = ['/fb.png', '/Insta.png', '/Twitter.png', '/Youtube.png'];
+    return links.map((link) => (
+      <img key={link} src={link} className="socail-link" />
+    ));
+  };
+  const contactLinks = () => {
+    let contactLinks = [
+      { title: 'Email', value: 'Leadconsultant@fynder.com', img: '/Email.png' },
+      { title: 'Hong Kong', value: '+852 55127188', img: '/location.png' },
+      { title: 'singapore', value: '+65 8333 4000', img: '/location.png' },
+    ];
+    return contactLinks.map((link) => (
+      <div key={link.title} className="footer-contact-link-item">
+        <img className="footer-contact-link-img" src={link.img} />
+        <h4>{link.title}</h4>
+        <p>{link.value}</p>
+      </div>
+    ));
+  };
 
-   const supportPageLinks = ()=>{
-    let pages = ['About Us','Services','Contact Us','FAQ','Blog'];
-    return pages.map((page)=><div className="page-link">{page}</div>)
-   }
-  const socialLinks = ()=> {
-   let links = ['/fb.png','/Insta.png','/Twitter.png','/Youtube.png'];
-   return links.map((link)=><img  src={link} className="socail-link"/>)
-  }
-    const contactLinks = () => {
-        let contactLinks = [{ title: 'Email', value: 'Leadconsultant@fynder.com',img:'/Email.png' }, { title: 'Hong Kong', value: '+852 55127188', img:'/location.png' }, { title: 'singapore', value: '+65 8333 4000',img:'/location.png' }]
-        return contactLinks.map((links) =>
-         <div className="footer-contact-link-item" >
-            <img className="footer-contact-link-img" src={links.img}/>
-            <h4>{links.title}</h4>
-            <p>{links.value}</p>
-        </div>)
-  }
+  const privacyLinks = () => {
+    let privacyLinks = [
+      'Privacy Policy',
+      'Terms and Conditions',
+      'Cookie Policy',
+    ];
+    return privacyLinks.map((privacyLink) => (
+      <div key={privacyLink}>{privacyLink}</div>
+    ));
+  };
 
+  return (
+    <>
+      <div className="footer-container">
+        <div className="footer-contact-page-container">
+          <Logo></Logo>
+          <div className="footer-contact-page-links-container">
+            {supportPageLinks()}
+          </div>
+          <div className="footer-socialMedia">{socialLinks()}</div>
+        </div>
+        <div className="footer-privacy-contact-links-container">
+          <Divider></Divider>
 
-  const privacyLinks=()=>{
-    let privacyLinks=['Privacy Policy','Terms and Conditions','Cookie Policy']
-    return privacyLinks.map(privacyLink=>
-        <div>{privacyLink}</div>
-    )
-  }
+          <div className="footer-privacy-links-container">
+            <div>&#xA9; 2024 Fynder Talent.All rights reserved </div>
+            {privacyLinks()}
+          </div>
 
-    return (
-        <>
-            <div className="footer-container">
-                <div className="footer-contact-page-container">
-                    <Logo></Logo>
-                    <div className='footer-contact-page-links-container'>
-                        {supportPageLinks()}
-                      </div>
-                    <div className="footer-socialMedia">
-                        {socialLinks()}
-                        </div>
-                </div>
-                <div className="footer-privacy-contact-links-container">
-                    <Divider></Divider>
-
-                    <div className="footer-privacy-links-container">
-                    <div>&#xA9; 2024 Fynder Talent.All rights reserved </div>
-                        {privacyLinks()}
-                    </div>
-
-                    <div className="footer-conatact-link-container">
-                      {contactLinks()}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+          <div className="footer-conatact-link-container">{contactLinks()}</div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,50 +1,56 @@
-'use client'
+'use client';
 import React, { use, useEffect, useState } from 'react';
 import { Logo } from '../Logo';
-import {Links} from '../../Utils/Links'
+import { Links } from '../../Utils/Links';
 
-
-import './index.css'
+import './index.css';
 import { Hamburger } from '../hamburger';
 
 const Header: React.FC = () => {
-  
-  const [isHamburgerOpen,setIsHamburgerOpen]=useState(false)
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
-
-  const handleNavLinkClick=(url)=>{
+  const handleNavLinkClick = (url: string) => {
     window.open(url, '_blank');
-  }
+  };
 
-  const menus = ()=>{
-    let links = [{name:'About',url:Links.NAV_LINKS.AboutUS},{name:'Offering',url:Links.NAV_LINKS.Offerings},{name:'Resources',url:Links.NAV_LINKS.Resources}]
-    return links.map((link)=>    <div className='nav-link' onClick={()=>handleNavLinkClick(link.url)}>{link.name} </div>
-  )
-  }
+  const menus = () => {
+    let links = [
+      { name: 'About', url: Links.NAV_LINKS.AboutUS },
+      { name: 'Offering', url: Links.NAV_LINKS.Offerings },
+      { name: 'Resources', url: Links.NAV_LINKS.Resources },
+    ];
+    return links.map((link) => (
+      <div
+        key={link.name}
+        className="nav-link"
+        onClick={() => handleNavLinkClick(link.url)}
+      >
+        {link.name}{' '}
+      </div>
+    ));
+  };
 
-
-  const hamburgerClicked=()=>{
-    let hamburger=document.querySelector('.hamburger')
-    if(!isHamburgerOpen){
-       hamburger?.classList.add("active")  
+  const hamburgerClicked = () => {
+    let hamburger = document.querySelector('.hamburger');
+    if (!isHamburgerOpen) {
+      hamburger?.classList.add('active');
+    } else {
+      hamburger?.classList.remove('active');
     }
-    else{
-      hamburger?.classList.remove("active")
-    }
-      setIsHamburgerOpen(!isHamburgerOpen)
-      
-  }
+    setIsHamburgerOpen(!isHamburgerOpen);
+  };
 
-  return <div className='flex nav-bar'>
-<div className='hamburgerContainer'>
-    <Logo></Logo>
-    <Hamburger onClick={hamburgerClicked}/>
+  return (
+    <div className="flex nav-bar">
+      <div className="hamburgerContainer">
+        <Logo></Logo>
+        <Hamburger onClick={hamburgerClicked} />
+      </div>
+      <div className={`nav-links ${isHamburgerOpen ? 'open' : ' '}`}>
+        {menus()}
+      </div>
     </div>
-    <div className={`nav-links ${isHamburgerOpen? 'open':' '}`}>
-    {menus()}
-    </div>
-    
-    </div>;
+  );
 };
 
 export default Header;
